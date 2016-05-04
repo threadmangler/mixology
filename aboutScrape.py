5ef1f40e0684d73f4d99546c8ce9bf022fc5bb67
threadmangler <threadmangler@users.noreply.github.com>
2016-05-03 20:18:48 -0400

    Added files via upload

--- aboutScrape.py
+++ aboutScrape.py
@@ -8,20 +8,23 @@ array = []
 with open("urls.txt", "r") as f:
   for line in f:
     array.append(line.strip())
-#    print array
+# print array
 
 #url = "http://cocktails.about.com/od/ginrecipes/r/floradora_cktl.htm"
 
 def scraping_function(url):
     # do stuff from OP's script
-    soup = BeautifulSoup(requests.get(url).text)
+    soup = BeautifulSoup(requests.get(url).text)    
 
-#print soup.find("ul", {"class": "ingredients-list"}).findAll("li")
-#ingrs = soup.find("ul", {"class": "ingredients-list"}).findAll("li")
-    print soup.findAll("li", {"itemprop": "ingredients"})     
+#    print soup.findAll("li", {"itemprop": "ingredients"})
 
-#for eaching in soup.findAll("li", {"itemprop": "ingredients"}):
-#    print eaching.parent.next_sibling.findAll('a')
+    for eaching in soup.findAll("li", {"itemprop": "ingredients"}):
+        inglist = eaching.findAll('a')
+        for a_tag in inglist:
+#            print eaching.findAll('a')
+            for inside in a_tag:
+                ingred = a_tag.contents
+                print ingred[0]
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=1) as e:
     # go through the url list 20 items at a time
